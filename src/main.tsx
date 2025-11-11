a5043778f9b1691d3115ae0d323327c858c0a2cb
Lukasz <setlaklukasz@gmail.com>
2025-11-11 23:52:30 +0100

    Add LESS support and refactor styles to use LESS files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 ﻿import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.less'
 import App from './App'
 
 const rootElement = document.getElementById('root')
