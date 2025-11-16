3c65b07ef1910bdc6f89a2ea9ae3486924b56474
Lukasz <setlaklukasz@gmail.com>
2025-11-16 14:35:59 +0100

    Update DarkVeil component usage with additional props for enhanced animation effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 ﻿import './App.css'
-import DarkVeil from "./components/layout/background/DarkVeil.tsx";
+// import DarkVeil from "./components/layout/background/DarkVeil.tsx";
+import DarkVeil from "./components/DarkVeil/DarkVeil.tsx";
 
 function App() {
     return (
         <div className="w-full h-full">
-            <DarkVeil />
+            <DarkVeil 
+                hueShift={180}
+                speed={0.25}
+                warpAmount={1}
+                resolutionScale={1}
+            />
         </div>
     )
 }
