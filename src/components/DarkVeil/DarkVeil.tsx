dce9e7679c1908b34a9cddf1c4b2fe9b64d71e65
Lukasz <setlaklukasz@gmail.com>
2025-11-16 14:35:50 +0100

    Add DarkVeil component and hooks for animated background effects

---
+++ src/components/DarkVeil/DarkVeil.tsx
@@ -0,0 +1,16 @@
+﻿import React from "react";
+import useDarkVeilGL from "./useDarkVeilGL";
+import type {DarkVeilProps} from "./types";
+
+export const DarkVeil: React.FC<DarkVeilProps> = (props) => {
+    const canvasRef = useDarkVeilGL(props);
+
+    return (
+        <canvas
+            ref={canvasRef}
+            className="w-full h-full block"
+        />
+    );
+};
+
+export default DarkVeil;
